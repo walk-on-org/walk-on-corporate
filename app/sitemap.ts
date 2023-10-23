84fcba2c5d3c5de9e8025e0f2a733c5ca8a6521c
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2023-10-23 16:51:38 +0900

    feature：sitemapとrobotstxtを作成

---
+++ app/sitemap.ts
@@ -0,0 +1,46 @@
+import { MetadataRoute } from 'next';
+import { getNewsList, getRecuritList } from './_libs/microcms';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const newsList = await getNewsList();
+  const recruitList = await getRecuritList();
+
+  return [
+    {
+      url: `${process.env.BASE_URL}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/news`,
+      lastModified: new Date(),
+    },
+    ...newsList.contents.map((row) => ({
+      url: `${process.env.BASE_URL}/news/${row.id}`,
+      lastModified: new Date(),
+    })),
+    {
+      url: `${process.env.BASE_URL}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/service`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/company`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.BASE_URL}/recruit`,
+      lastModified: new Date(),
+    },
+    ...recruitList.contents.map((row) => ({
+      url: `${process.env.BASE_URL}/recruit/${row.id}`,
+      lastModified: new Date(),
+    })),
+    {
+      url: `${process.env.BASE_URL}/contact`,
+      lastModified: new Date(),
+    },
+  ];
+}
